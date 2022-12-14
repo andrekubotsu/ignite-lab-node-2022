9dcd6bb5fc5ca48f2944a229287a9b2c8567e87e
Andre Kubotsu <contato201@gmail.com>
2022-12-14 11:58:00 -0300

    feat: persistencia e rotas iniciais

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
-
-import { MailService } from './mail/mail.service';
-
-@Controller()
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notification-body';
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: MailService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.sendEmail();
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { content, category, recipientId } = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
   }
 }
